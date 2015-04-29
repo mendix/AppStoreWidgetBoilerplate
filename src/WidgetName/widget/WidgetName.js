61d0acbf0c03502a548fdcd26473ecf90bb995d8
Johan Flint <johan.flint@mendix.com>
2015-04-29 12:56:45 +0200

    Remove space after the function keyword

--- src/WidgetName/widget/WidgetName.js
+++ src/WidgetName/widget/WidgetName.js
@@ -22,7 +22,7 @@ define([
     "dojo/_base/declare", "mxui/widget/_WidgetBase", "dijit/_TemplatedMixin",
     "mxui/dom", "dojo/dom", "dojo/query", "dojo/dom-prop", "dojo/dom-geometry", "dojo/dom-class", "dojo/dom-style", "dojo/dom-construct", "dojo/_base/array", "dojo/_base/lang", "dojo/text", "dojo/html", "dojo/_base/event",
     "WidgetName/lib/jquery-1.11.2.min", "dojo/text!WidgetName/widget/template/WidgetName.html"
-], function (declare, _WidgetBase, _TemplatedMixin, dom, dojoDom, domQuery, domProp, domGeom, domClass, domStyle, domConstruct, dojoArray, lang, text, html, event, _jQuery, widgetTemplate) {
+], function(declare, _WidgetBase, _TemplatedMixin, dom, dojoDom, domQuery, domProp, domGeom, domClass, domStyle, domConstruct, dojoArray, lang, text, html, event, _jQuery, widgetTemplate) {
     "use strict";
 
     var $ = _jQuery.noConflict(true);
@@ -44,19 +44,19 @@ define([
         _alertDiv: null,
 
         // dojo.declare.constructor is called to construct the widget instance. Implement to initialize non-primitive properties.
-        constructor: function () {
+        constructor: function() {
             this._handles = [];
         },
 
         // dijit._WidgetBase.postCreate is called after constructing the widget. Implement to do extra setup work.
-        postCreate: function () {
+        postCreate: function() {
             console.log(this.id + ".postCreate");
             this._updateRendering();
             this._setupEvents();
         },
 
         // mxui.widget._WidgetBase.update is called when context is changed or initialized. Implement to re-render and / or fetch data.
-        update: function (obj, callback) {
+        update: function(obj, callback) {
             console.log(this.id + ".update");
 
             this._contextObj = obj;
@@ -67,35 +67,35 @@ define([
         },
 
         // mxui.widget._WidgetBase.enable is called when the widget should enable editing. Implement to enable editing if widget is input widget.
-        enable: function () {},
+        enable: function() {},
 
         // mxui.widget._WidgetBase.enable is called when the widget should disable editing. Implement to disable editing if widget is input widget.
-        disable: function () {},
+        disable: function() {},
 
         // mxui.widget._WidgetBase.resize is called when the page's layout is recalculated. Implement to do sizing calculations. Prefer using CSS instead.
-        resize: function (box) {},
+        resize: function(box) {},
 
         // mxui.widget._WidgetBase.uninitialize is called when the widget is destroyed. Implement to do special tear-down work.
-        uninitialize: function () {
+        uninitialize: function() {
             // Clean up listeners, helper objects, etc. There is no need to remove listeners added with this.connect / this.subscribe / this.own.
         },
 
         // We want to stop events on a mobile device
-        _stopBubblingEventOnMobile: function (e) {
+        _stopBubblingEventOnMobile: function(e) {
             if (typeof document.ontouchstart !== "undefined") {
                 event.stop(e);
             }
         },
 
         // Attach events to HTML dom elements
-        _setupEvents: function () {
+        _setupEvents: function() {
 
-            this.connect(this.colorSelectNode, "change", function (e) {
+            this.connect(this.colorSelectNode, "change", function(e) {
                 // Function from mendix object to set an attribute.
                 this._contextObj.set(this.backgroundColor, this.colorSelectNode.value);
             });
 
-            this.connect(this.infoTextNode, "click", function (e) {
+            this.connect(this.infoTextNode, "click", function(e) {
 
                 // Only on mobile stop event bubbling!
                 this._stopBubblingEventOnMobile(e);
@@ -108,10 +108,10 @@ define([
                             actionname: this.mfToExecute,
                             guids: [this._contextObj.getGuid()]
                         },
-                        callback: function (obj) {
+                        callback: function(obj) {
                             //TODO what to do when all is ok!
                         },
-                        error: lang.hitch(this, function (error) {
+                        error: lang.hitch(this, function(error) {
                             console.log(this.id + ": An error occurred while executing microflow: " + error.description);
                         })
                     }, this);
@@ -122,7 +122,7 @@ define([
         },
 
         // Rerender the interface.
-        _updateRendering: function () {
+        _updateRendering: function() {
 
             this.colorSelectNode.disabled = this.readOnly;
             this.colorInputNode.disabled = this.readOnly;
@@ -146,7 +146,7 @@ define([
         },
 
         // Handle validations.
-        _handleValidation: function (_validations) {
+        _handleValidation: function(_validations) {
             this._clearValidations();
 
             var _validation = _validations[0],
@@ -163,13 +163,13 @@ define([
         },
 
         // Clear validations.
-        _clearValidations: function () {
+        _clearValidations: function() {
             domConstruct.destroy(this._alertdiv);
             this._alertdiv = null;
         },
 
         // Show an error message.
-        _showError: function (message) {
+        _showError: function(message) {
             if (this._alertDiv !== null) {
                 html.set(this._alertDiv, message);
                 return true;
@@ -182,19 +182,19 @@ define([
         },
 
         // Add a validation.
-        _addValidation: function (message) {
+        _addValidation: function(message) {
             this._showError(message);
         },
 
         // Reset subscriptions.
-        _resetSubscriptions: function () {
+        _resetSubscriptions: function() {
             var _objectHandle = null,
                 _attrHandle = null,
                 _validationHandle = null;
 
             // Release handles on previous object, if any.
             if (this._handles) {
-                this._handles.forEach(function (handle, i) {
+                this._handles.forEach(function(handle, i) {
                     mx.data.unsubscribe(handle);
                 });
                 this._handles = [];
@@ -205,7 +205,7 @@ define([
 
                 _objectHandle = this.subscribe({
                     guid: this._contextObj.getGuid(),
-                    callback: lang.hitch(this, function (guid) {
+                    callback: lang.hitch(this, function(guid) {
                         this._updateRendering();
                     })
                 });
@@ -213,7 +213,7 @@ define([
                 _attrHandle = this.subscribe({
                     guid: this._contextObj.getGuid(),
                     attr: this.backgroundColor,
-                    callback: lang.hitch(this, function (guid, attr, attrValue) {
+                    callback: lang.hitch(this, function(guid, attr, attrValue) {
                         this._updateRendering();
                     })
                 });
@@ -229,6 +229,6 @@ define([
         }
     });
 });
-require(["WidgetName/widget/WidgetName"], function () {
+require(["WidgetName/widget/WidgetName"], function() {
     "use strict";
 });
