0553e57b920f8867403768bca61ab367caac4574
Achiel <achiel.van.der.mandele@mendix.com>
2015-06-15 15:57:41 +0200

    Support loading multiple jqueries
    So, the problem here is that jquery is a library that uses define (AMD
    style), but passes it an explicit string which is non unique, "jquery".
    This means that if you load different jquery versions they will all
    try to define themselves as "jquery".
    
    Using the unminified jquery version (we bundle when creating MDAs anyway)
    and removing the jquery identifier solves this problem, as the define
    will then define itself as the path to the file.

--- src/WidgetName/widget/WidgetName.js
+++ src/WidgetName/widget/WidgetName.js
@@ -21,11 +21,11 @@
 define([
     'dojo/_base/declare', 'mxui/widget/_WidgetBase', 'dijit/_TemplatedMixin',
     'mxui/dom', 'dojo/dom', 'dojo/query', 'dojo/dom-prop', 'dojo/dom-geometry', 'dojo/dom-class', 'dojo/dom-style', 'dojo/dom-construct', 'dojo/_base/array', 'dojo/_base/lang', 'dojo/text', 'dojo/html', 'dojo/_base/event',
-    'WidgetName/lib/jquery-1.11.2.min', 'dojo/text!WidgetName/widget/template/WidgetName.html'
+    'WidgetName/lib/jquery-1.11.2', 'dojo/text!WidgetName/widget/template/WidgetName.html'
 ], function (declare, _WidgetBase, _TemplatedMixin, dom, dojoDom, domQuery, domProp, domGeom, domClass, domStyle, domConstruct, dojoArray, lang, text, html, event, _jQuery, widgetTemplate) {
     'use strict';
 
-    var $ = jQuery.noConflict(true);
+    var $ = _jQuery.noConflict(true);
     
     // Declare widget's prototype.
     return declare('WidgetName.widget.WidgetName', [_WidgetBase, _TemplatedMixin], {
